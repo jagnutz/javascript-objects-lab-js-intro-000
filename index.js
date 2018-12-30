21f23d09031a478d08c6331aa0ce8a1654975f53
Michael <chaphlux@yahoo.com>
2018-12-30 00:30:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,9 +4,3 @@ function updateObjectWithKeyAndValue (recipes,key,value) {
  recipes[key] = value
   return recipes
 }
-
-function updateObjectWithKeyAndValue (recipes,key,value){
-  const newRecipes = Object.assign({},recipes,{[key]:value})
-  delete newRecipes[key]
-  return newRecipes
-}
